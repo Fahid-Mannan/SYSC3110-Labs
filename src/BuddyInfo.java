9bda4eb5a66882af2d634781e00159129c027c56
fahidmannan <fahidmannan@cb5107-27.labs.sce.carleton.ca>
2017-09-25 09:18:31 -0400

    Added a print statement to print Address Book

---
+++ src/BuddyInfo.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class BuddyInfo {
+	private String name;
+	private String address;
+	private long phoneNum;
+	
+	private Map<String, BuddyInfo> addressBook;
+	
+	public BuddyInfo() { 
+		addressBook = new HashMap<>(); 
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public long getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(long phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		BuddyInfo buddy = new BuddyInfo();
+		buddy.setName("Homer");
+		System.out.println("Hello " + buddy.getName() + ".");
+	}
+
+}
