c589989ed7910f12ac5e3684a519aa97e1faaf5e
fahidmannan <fahidmannan@cb5107-27.labs.sce.carleton.ca>
2017-09-25 09:45:03 -0400

    Added and removed a buddy

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,17 +1,21 @@
 import java.util.*;
 
 public class AddressBook {
-	private Map<String, BuddyInfo> addressBook;
+	private static Map<String, BuddyInfo> addressBook;
 	
 	public AddressBook() {
 		addressBook = new HashMap<String, BuddyInfo>(); 
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Address Book");
+	
+	public static void insert(BuddyInfo buddy) {
+		addressBook.put(buddy.getName(), buddy);
 	}
-
+	
+	public static void remove(BuddyInfo buddy) {
+		addressBook.remove(buddy.getName());
+	}
+	
 	public Map<String, BuddyInfo> getAddressBook() {
 		return addressBook;
 	}
@@ -19,5 +23,15 @@ public class AddressBook {
 	public void setAddressBook(Map<String, BuddyInfo> addressBook) {
 		this.addressBook = addressBook;
 	}
-
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Address Book");
+		BuddyInfo buddy = new BuddyInfo();
+		buddy.setName("Fahid");
+		
+		insert(buddy);
+		remove(buddy);
+	}
+	
 }
